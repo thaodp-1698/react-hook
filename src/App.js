6e153f69797b00f12a8c6e70053fba880c455751
Thao ham <duong.phuong.thao@sun-asterisk.com>
2021-09-19 15:01:59 +0700

    react hook useEffect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import PostList from './components/PostList';
 
 function App() {
+  const [postList, setPostList] = useState([]);
+
+  useEffect(() => {
+    async function fetchPostList() {
+      try {
+        const requestUrl = 'http://js-post-api.herokuapp.com/api/posts?_limit=10&_page=1';
+        const response = await fetch(requestUrl);
+        const responseJSON = await response.json();
+
+        const {data} = responseJSON;
+        setPostList(data);
+      } catch (error) {
+        console.log(error.message);
+      }
+    }
+
+    fetchPostList();
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Post list</h1>
+      <PostList  posts={postList} />
     </div>
   );
 }
